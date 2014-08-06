5ff1e3bea1c3a3dc0c44466e613bd2532532d44d
cm <cm@localhost.localdomain>
2014-08-07 06:36:46 +0800

    test.c

--- test.c
+++ test.c
@@ -1,18 +1,49 @@
-#include <stdio.h>
 #include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+#define BUFSIZE 512
+
+void ioread(){
+	int n;
+	char buf[BUFSIZE];
+
+	while ( ( n = read(STDIN_FILENO,buf,BUFSIZE)) > 0 ){
+		if ( write(STDOUT_FILENO,buf,n) != n ){
+			fprintf(stderr,"read error\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	if ( n < 0 ){
+		fprintf(stderr,"read error\n");
+		exit(EXIT_FAILURE);
+	}
+}
 
 int main(int argc, char *argv[]) {
 	DIR *dp;
 	struct dirent *dirp;
 
-	if ((dp = opendir(".")) == NULL) {
-		fprintf(stderr,"file open error");
+	if ( argc < 2 ){
+		fprintf(stderr,"./a.out /filepath");
+		exit(EXIT_FAILURE);
+	}
+	if ((dp = opendir(argv[1])) == NULL) {
+		fprintf(stderr,"file open error %s\n",strerror(errno));
+		exit(EXIT_FAILURE);
 	}
 	while ((dirp = readdir(dp)) != NULL) {
 		printf("%s\n", dirp->d_name);
 	}
-	close(dp);
+	closedir(dp);
 
 	printf("Hello Eclipse C");
+
+	ioread();
 	return 0;
 }
+
